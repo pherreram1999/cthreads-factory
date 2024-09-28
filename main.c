ca39643856ce2c0b34e69be4a7f9fa97bfbdced4
Pedro Alonso <palhm.99@gmail.com>
2024-09-28 02:57:09 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "types.h"
+
+#define MAX_INJECTIONS  5
+
+// definimos mutex
+pthread_mutex_t mutexOne;
+
+// variables globales que se compartes
+
+listnode *cintaInyecciones = nullptr;
+listnode *cintaEmbalajes = nullptr;
+
+
+listnode *nuevaLista() {
+    return (listnode *)malloc(sizeof(listnode));
+}
+
+/**
+ * Retorna el ultimo nodo de la lista
+ * @param lista
+ * @return
+ */
+listnode *getLastNode(listnode **lista) {
+    listnode *nav = *lista;
+    while (nav != nullptr) {
+        nav = nav->next;
+    }
+    return nav;
+}
+
+/**
+ * Inserta un nuevo id, creando su nodo en la lista
+ * @param lista
+ * @param idinyeccion
+ */
+void insertIntoList(listnode **lista,int idinyeccion) {
+    if (*lista == nullptr) { // primer nodo
+        listnode *nuevaInyeccion = nuevaLista();
+        nuevaInyeccion->id = idinyeccion;
+        *lista = nuevaInyeccion;
+        return;
+    }
+
+    listnode *nav = *lista;
+    while(nav->next != nullptr) {
+        nav = nav->next; // nos colocamos en el ultimo nodo
+    }
+    listnode *nuevaInyeccion = nuevaLista();
+    nuevaInyeccion->id = idinyeccion;
+    nuevaInyeccion->prev = nav;
+    nav->next = nuevaInyeccion;
+}
+
+
+void appendNodeToList(listnode **lista,listnode *node) {
+    if (*lista == nullptr) {
+        // primer elemento de la lista
+        *lista = node;
+        return;
+    }
+    listnode *lastNode = getLastNode(lista);
+    lastNode->next = node;
+    node->next = nullptr;
+}
+
+/**
+ * remueve un nodo de la lista y regresa la direccion del nodo removido
+ * @param lista
+ * @param id
+ * @return nodo removido de la lista
+ */
+listnode* removeNodeFromList(listnode **lista, int id) {
+    if (*lista == nullptr) {
+        return nullptr; // no esta en la lista
+    }
+
+    listnode *nav = *lista;
+    // si el nodo queremos es el primero
+    if(nav->id == id) {
+        // recorremos la lista
+        *lista = nav->next; // este movimiento puede dejar nulo la lista
+
+        if(*lista == nullptr) {
+            // si la lista actual es no null,
+            // ajustamos el prev
+            (*lista)->prev = nullptr; // ya no apunta nada
+        }
+
+        // se libera las relaciones del nodo encontrado
+        nav->next = nullptr;
+        nav->prev = nullptr;
+        return nav; // regresamos
+    }
+
+    // recorremos hasta encontrarlo
+
+    while(nav != nullptr && nav->id != id) {
+        nav = nav->next;
+    }
+
+    // si despues de recorrerlo es nulo, entonces no existe
+    if(nav == nullptr) {
+        return nullptr;
+    }
+
+    // recorremos el puntero de prev
+
+    nav->prev->next = nav->next;
+
+    // si tenemos next recorremos su prev
+    if(nav->next != nullptr) {
+        nav->next->prev = nav->prev;
+    }
+
+    // liberamos el nodo
+    nav->next = nullptr;
+    nav->prev = nullptr;
+
+    return nav;
+}
+
+
+/**
+ * llena la lista con ids de las "inyecciones"
+ * @param arg
+ * @return
+ */
+void *llenadoCinta(void *arg) {
+    // recibimos el apuntador del apuntador de la lista
+    for(int i = 1; i <= MAX_INJECTIONS; i++) {
+        // por cada inyeccion evitamos que se toque la lista en paralelo
+        pthread_mutex_lock(&mutexOne);
+        insertIntoList(&cintaInyecciones,i);
+        pthread_mutex_unlock(&mutexOne);
+    }
+    /*listnode *nav = &(*data);
+
+    */
+
+}
+
+void verLista(listnode **lista) {
+    listnode *nav = *lista;
+    while(nav != nullptr) {
+        printf("inyeccion id: %d\n",nav->id);
+        nav = nav->next;
+    }
+}
+
+/**
+ * Saca valores de la cinta de inyecciones y lo coloca en la de embalaje
+ * @param arg
+ * @return
+ */
+void *embalar(void *arg) {
+    // siempre obtenemos el primer elemento de lista
+    listnode * removedNode = removeNodeFromList(&cintaInyecciones,(cintaInyecciones)->id);
+    printf("removed id: %d\n",removedNode->id);
+    // ese elemento loa agregamos a la lista de embalajes
+    appendNodeToList(&cintaEmbalajes,removedNode);
+    return nullptr;
+}
+
+
+
+
+int main(void) {
+    // declaramos los identificadores de los threads
+    pthread_t cintaTransportadoraThread, embalajesThread;
+    // inciamos los procesos en paralelo
+    pthread_create(&cintaTransportadoraThread,nullptr,llenadoCinta,nullptr);
+    // esperamos los resultados
+    pthread_join(cintaTransportadoraThread,nullptr);
+
+
+    /*removeNodeFromList(&cintaInyecciones,3);
+    removeNodeFromList(&cintaInyecciones,4);*/
+
+    verLista(&cintaInyecciones);
+
+    //embalar(nullptr);
+    // en otro proceso mostramos un cotador en otro thead
+
+    return 0;
+}
---
+++ types.h
@@ -0,0 +1,16 @@
+//
+// Created by sistemas on 28/09/24.
+//
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef struct nodo {
+    int id;
+    struct nodo *next;
+    struct nodo *prev;
+} listnode;
+
+#endif //TYPES_H
+
+
